045823fb5d38a81800b972645dd51d895fa0b0a7
soufiane odf <soufiane.odf@gmail.com>
2020-06-08 19:03:40 +0100

    Remove UserService and related code

--- src/main/java/org/mvnsearch/ssh/demo/action/TableAction.java
+++ src/main/java/org/mvnsearch/ssh/demo/action/TableAction.java
@@ -1,13 +1,8 @@
 package org.mvnsearch.ssh.demo.action;
 
 import com.opensymphony.xwork2.ActionSupport;
-import org.mvnsearch.ssh.demo.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
 
 public class TableAction extends ActionSupport {
-
-    @Autowired
-    private UserService userService;
 
     @Override
     public String execute() throws Exception {
--- src/main/java/org/mvnsearch/ssh/demo/service/UserService.java
+++
@@ -1,9 +0,0 @@
-package org.mvnsearch.ssh.demo.service;
-
-/**
- * user service 
- *
- * @author linux_china
- */
-public interface UserService {
-}
